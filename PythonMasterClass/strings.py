00cfddf92f91b8efb3406a79684a4a7d7a95d1fe
Harmanvir Singh <59752492+harmanvir1995@users.noreply.github.com>
2022-03-07 20:22:52 -0500

    PythonPractice
    Python Practice

---
+++ PythonMasterClass/strings.py
@@ -0,0 +1,32 @@
+print("""Number 1 \tThe Larch"
+Number 2 \tThe Horse Chestnut""")
+print()
+print("Today is a good day to learn Python")
+print("hello " + "world")
+print("hello", "friends")
+
+greeting = "Hello"
+name = "Harman"
+
+# If we want the space we can add that too
+print(greeting + " " + name)
+splitString = "This string has been \nsplit over \nseveral times"
+print(splitString)
+print("this is the pet shop \" This is the \" best way \\ to learn the python")
+print("""This is a "cool feature" from Pyhton """)
+anotherString = """I can use this cool
+feature as well 
+which is not intoduced \
+in java and \
+C C++"""
+print(anotherString)
+print("C:\\Users\\Narman\\notes")
+print(r"C:\Users\Narman\notes")
+age = 26
+print(age)
+print(type(age))
+print("Changing the value of the variable \"age\"")
+age = "2 years"
+print(age)
+print(type(age))
+
